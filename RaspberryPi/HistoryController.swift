943336f89e90977a19b791a09f6fbd5ddeef603a
Zaeem Siddiq <zaeem.siddiq@gmail.com>
2016-10-13 20:41:19 +1100

    comments added

--- RaspberryPi/HistoryController.swift
+++ RaspberryPi/HistoryController.swift
@@ -6,6 +6,9 @@
 //  Copyright © 2016 Zaeem Siddiq. All rights reserved.
 //
 
+/*
+ This controller gets the time histories from the sensors and displays. The controller is dealing with 2 methods, one is getting the time based temperatures and the other is getting lst N values from the sensor.
+ */
 import UIKit
 
 class HistoryController: UIViewController, ServiceDelegate {
@@ -27,24 +30,42 @@ class HistoryController: UIViewController, ServiceDelegate {
         
         let tempService = HttpRequestService(delegate: self)
         tempService.getTempBydate(String(fromDateUtc), endUtc: String(toDateUtc))
-        
     }
     
     @IBAction func getLastN(sender: AnyObject) {
-        let num = textLastN.text
-        let tempService = HttpRequestService(delegate: self)
-        tempService.getTempN(Int(num!)!)
+        var isValid: Bool = true
+        let num = Int(textLastN.text!)
+        if textLastN.text == "" {
+            isValid = false
+             generateNotification("Error", message: "Please enter a number")
+        } else if num == nil {
+            isValid = false
+            generateNotification("Error", message: "Please enter a valid number")
+        }
+        if isValid {
+             let tempService = HttpRequestService(delegate: self)
+        }
+        
     }
     
+    // this method generates a notification on.
+    func generateNotification(title: String, message: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .Alert)
+        let alertAction = UIAlertAction(title: "OK", style: .Default, handler: nil)
+        alertController.addAction(alertAction)
+        self.presentViewController(alertController, animated: true, completion: nil)
+    }
     
     func didReceiveError(error:String) {
         
     }
+    
+    // this method gets called from the HTTPRequestService
     func serviceComplete(data: NSData, functionCall: String) {
         if functionCall == HttpRequestService.GET_TEMP_N {
             do {
                 if let jsonResult = try NSJSONSerialization.JSONObjectWithData(data, options: []) as? NSArray {
-                    
+                    // parsing the array and updating the regular GUI in the thread
                     if let temperatures = jsonResult[0]["values"] as? NSArray {
                         for temperature in temperatures {
                             print (Double(temperature["timestamp"]! as! Double))
